fa7c4adf9d0995382b01d6a316dcd4114cd085c1
Muhammad Ahmed <muhammadahmed@NIS-PK306s.local>
2023-06-06 12:56:44 +0500

    Add Vector icon and configure pre commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import {Text} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 import {BaseSafeAreaView} from "components";
+import {Text} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const App = () => (
   <NavigationContainer>
     <BaseSafeAreaView>
-      <Text>Hellow World</Text>
+      <Icon.Button name="facebook" backgroundColor="#3b5998">
+        <Text style={{fontFamily: "Arial", fontSize: 15}}>
+          Login with Facebook
+        </Text>
+      </Icon.Button>
     </BaseSafeAreaView>
   </NavigationContainer>
 );
