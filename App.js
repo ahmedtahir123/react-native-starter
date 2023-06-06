91f0b957bc235c5d6d613498b46884c30c0b82bb
Muhammad Ahmed <muhammadahmed@NIS-PK306s.local>
2023-06-06 13:25:52 +0500

    Checking pre cmmit

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import {Text} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const App = () => (
-  <NavigationContainer>
-    <BaseSafeAreaView>
-      <Icon.Button name="facebook" backgroundColor="#3b5998">
-        <Text style={{fontFamily: "Arial", fontSize: 15}}>
-          Login with Facebook
-        </Text>
-      </Icon.Button>
-    </BaseSafeAreaView>
-  </NavigationContainer>
-);
+    <NavigationContainer>
+      <BaseSafeAreaView>
+        <Icon.Button name="facebook" backgroundColor="#3b5998">
+          <Text style={{fontFamily: "Arial", fontSize: 15}}>
+            Login with Facebook
+          </Text>
+        </Icon.Button>
+      </BaseSafeAreaView>
+    </NavigationContainer>
+  );
 
 export default App;
